30091f06a2104a5f14c151461ec7f230be18319e
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2024-06-13 21:58:16 +0300

    Ajout de la fonction getAllFilesWOnedByGroup

--- GroupManager/Group.php
+++ GroupManager/Group.php
@@ -14,11 +14,16 @@ class UserGroup {
     }
 
     public static function getAllUserInGroup($groupName) {
+        // Déclaration pour stocker la valeur de retour
         $USERS = [];
 
+        // Commande pour avoir tous les users dans le group donné
         $command = "getent group " .$groupName . " | cut -d: -f4 | tr ',' '\n'";
+
+        // LA sortie de la commande lorsqu'elle est executé
         $output = shell_exec($command);
 
+        // On split l'output pour avoirs les listes des users
         $USERS = explode(' ', $output);
 
         return $USERS;
@@ -31,8 +36,20 @@ class UserGroup {
         return $output;
     }    
 
-    public static function getAllFileOwnedBy($groupName, $pathFolder) {
-        $output = exec('sh ../scriptBash/findFolderOwner.sh '.$groupName . $pathFolder);
+    /* **************************************************************** */
+
+    public static function getAllFilesOwnedByGroup($groupName, $pathFolder) {
+        // Utilisation de escapeshellarg pour sécuriser la commande
+        $safePathFolder = escapeshellarg($pathFolder);
+        
+        // Construction de la commande pour rechercher les fichiers appartenant au groupe spécifié
+        $command = "find {$safePathFolder} -type f -group {$groupName}";
+        
+        // Exécution de la commande et capture de la sortie
+        $output = shell_exec($command);
+        
+        // Retour de la sortie sous forme de tableau
+        return explode("\n", $output);
     }
     
 
@@ -41,9 +58,9 @@ class UserGroup {
 $GROUPS                 = UserGroup::getAllUser();
 $USERS                  = UserGroup::getAllUserInGroup('sudo');
 $ID_ROOT                = UserGroup::getGroupId('root');
-$FOLDER_OWNED_BY_ROOT   = UserGroup::getAllFileOwnedBy('sudo', '~');
+$FOLDER_OWNED_BY_ROOT   = UserGroup::getAllFilesOwnedByGroup('misa2026', '.');
 
 // echo "sudoers \n"; var_dump($GROUPS);
 // echo $ID_ROOT;
-echo $FOLDER_OWNED_BY_ROOT;
+// var_dump( $FOLDER_OWNED_BY_ROOT );
 ?>
